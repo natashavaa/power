fe1a3875be5053b7ffc31ee5afcfd010f16717c0
SanvicenteCesar <cesarsvp17@gmail.com>
2020-03-09 16:16:31 -0400

    pacientes validados

--- src/app/pages/registropaciente/registropaciente.component.ts
+++ src/app/pages/registropaciente/registropaciente.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { PaatientInterface } from '../../models/patients.interface';
 import { UserInterface } from 'src/app/models/user.interface';
+import { DataApiService } from '../../services/data-api.service';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-registropaciente',
@@ -11,12 +13,35 @@ import { UserInterface } from 'src/app/models/user.interface';
 })
 export class RegistropacienteComponent implements OnInit {
 
-  constructor(private router: Router,  private authService: AuthService) { }
+  constructor(private router: Router,  private authService: AuthService, private dataApi: DataApiService, private app:AppComponent) { }
 
   selectedValue: string;
   step = 0;
 
   private patient: PaatientInterface = {
+    name: '',
+    dni: '',
+    age: 0,
+    sex: '',
+    statusC: '',
+    homeAddress: '',
+    occupation : '',
+    workAddress: '',
+    mobile: 0,
+    birthplace: '',
+    sentBy: '',
+    registradoPor: '',
+    dentalColor: '',
+    familyNumber: 0,
+    familyNumberHome: 0,
+    familyName: '',
+    password: '',
+    username: '',
+    mail: '',
+    userType: '',
+    phone: '',
+  };
+  private patientEncontrado: PaatientInterface = {
     name: '',
     dni: '',
     age: 0,
@@ -50,36 +75,45 @@ export class RegistropacienteComponent implements OnInit {
     userType: '',
     username: '',
     phone: ''
-  
-  
+
+
   };
   onRegisterPatient(): void {
-    this.patient.registradoPor  = this.user.name;
-    this.authService.registerPatient(
-      this.patient.name,
-      this.patient.dni,
-      this.patient.age,
-      this.patient.sex,
-      this.patient.statusC,
-      this.patient.homeAddress,
-      this.patient.occupation,
-      this.patient.workAddress,
-      this.patient.mobile,
-      this.patient.birthplace,
-      this.patient.sentBy,
-      this.patient.dentalColor,
-      this.patient.familyNumber,
-      this.patient.familyName,
-      this.patient.familyNumberHome,
-      this.patient.password,
-      this.patient.username,
-      this.patient.mail,
-      this.patient.userType,
-      this.patient.phone,
-      this.patient.registradoPor,
-    ).subscribe(patient => {
-      this.router.navigate(['pacientes']);
-     } );
+    if (this.patient.dni) {
+      this.dataApi.getPatientByDni(this.patient.dni).subscribe((patients: PaatientInterface) => {
+         this.patientEncontrado = patients;
+         if (!this.patientEncontrado) {
+          this.patient.registradoPor  = this.user.name;
+          this.authService.registerPatient(
+            this.patient.name,
+            this.patient.dni,
+            this.patient.age,
+            this.patient.sex,
+            this.patient.statusC,
+            this.patient.homeAddress,
+            this.patient.occupation,
+            this.patient.workAddress,
+            this.patient.mobile,
+            this.patient.birthplace,
+            this.patient.sentBy,
+            this.patient.dentalColor,
+            this.patient.familyNumber,
+            this.patient.familyName,
+            this.patient.familyNumberHome,
+            this.patient.password,
+            this.patient.username,
+            this.patient.mail,
+            this.patient.userType,
+            this.patient.phone,
+            this.patient.registradoPor,
+          ).subscribe(patient => {
+            this.router.navigate(['pacientes']);
+           } );
+         } else {
+          alert('Cedula Registrada');
+         }
+      });
+     }
     }
   setStep(index: number) {
     this.step = index;
@@ -104,6 +138,7 @@ export class RegistropacienteComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.app.mostrar = true;
     this.user = this.authService.getCurrentUser();
   }
 
