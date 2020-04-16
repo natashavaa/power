88b11fd1f343e92f87aaf855d0985489ceccbe75
SanvicenteCesar <cesarsvp17@gmail.com>
2020-04-16 16:15:25 -0400

    volviendo a local

--- src/app/services/data-api.service.ts
+++ src/app/services/data-api.service.ts
@@ -12,174 +12,174 @@ export class DataApiService {
   constructor( private http: HttpClient) {}
 
   getAllPatints() {
-    const urlApi = 'http://192.168.250.4:3000/patient' ;
+    const urlApi = 'http://localhost:3000/patient' ;
     return this.http.get(urlApi);
   }
   getPatientsByDoctor(name: string) {
-    const urlApi = `http://192.168.250.4:3000/patient/doctor${name}`;
+    const urlApi = `http://localhost:3000/patient/doctor${name}`;
     return this.http.get(urlApi);
   }
   getSeguimientoByOdontograma(id) {
-    const urlApi = `http://192.168.250.4:3000/seguimientoadiente/odontograma/${id}`;
+    const urlApi = `http://localhost:3000/seguimientoadiente/odontograma/${id}`;
     return this.http.get(urlApi);
   }
   getSeguimientoByOdontogramatodoslosdientes(id) {
-    const urlApi = `http://192.168.250.4:3000/seguimientodetalletodos/odontograma/${id}`;
+    const urlApi = `http://localhost:3000/seguimientodetalletodos/odontograma/${id}`;
     return this.http.get(urlApi);
   }
   getAllRecipePatient(id: string) {
-    const urlApi = `http://192.168.250.4:3000/recipe/patientrecipe${id}`;
+    const urlApi = `http://localhost:3000/recipe/patientrecipe${id}`;
     return this.http.get(urlApi);
   }
   getAllPresupustoPatient(id: string) {
-    const urlApi = `http://192.168.250.4:3000/presupuesto/patientpresupuesto${id}`;
+    const urlApi = `http://localhost:3000/presupuesto/patientpresupuesto${id}`;
     return this.http.get(urlApi);
   }
   getAllInformePatient(id: string) {
-    const urlApi = `http://192.168.250.4:3000/informemedico/patientinforme${id}`;
+    const urlApi = `http://localhost:3000/informemedico/patientinforme${id}`;
     return this.http.get(urlApi);
   }
   getAllconsultas() {
-    const urlApi = 'http://192.168.250.4:3000/consulta' ;
+    const urlApi = 'http://localhost:3000/consulta' ;
     return this.http.get(urlApi);
   }
   getAllconsultasByDoctor(doctor: string) {
-    const urlApi = `http://192.168.250.4:3000/consulta/doctor${doctor}` ;
+    const urlApi = `http://localhost:3000/consulta/doctor${doctor}` ;
     return this.http.get(urlApi);
   }
 
 
   getAllconsultasBypatient(id) {
-    const urlApi = `http://192.168.250.4:3000/consulta/patient${id}`;
+    const urlApi = `http://localhost:3000/consulta/patient${id}`;
     return this.http.get(urlApi);
   }
   getAllEvolutionBypatient(id) {
-    const urlApi = `http://192.168.250.4:3000/evolution/patient${id}`;
+    const urlApi = `http://localhost:3000/evolution/patient${id}`;
     return this.http.get(urlApi);
   }
   getAllHistorialBypatient(id) {
-    const urlApi = `http://192.168.250.4:3000/historialclinico/patient${id}`;
+    const urlApi = `http://localhost:3000/historialclinico/patient${id}`;
     return this.http.get(urlApi);
   }
   getAllconsultasHoy() {
-    const urlApi = 'http://192.168.250.4:3000/consulta/hoy' ;
+    const urlApi = 'http://localhost:3000/consulta/hoy' ;
     return this.http.get(urlApi);
   }
   getAllconsultasMes() {
-    const urlApi = 'http://192.168.250.4:3000/consulta/mes' ;
+    const urlApi = 'http://localhost:3000/consulta/mes' ;
     return this.http.get(urlApi);
   }
   getAllUser() {
-    const urlApi = 'http://192.168.250.4:3000/users' ;
+    const urlApi = 'http://localhost:3000/users' ;
     return this.http.get(urlApi);
   }
   getAllUserConPermiso() {
-    const urlApi = 'http://192.168.250.4:3000/users/doctorPermiso' ;
+    const urlApi = 'http://localhost:3000/users/doctorPermiso' ;
     return this.http.get(urlApi);
   }
     getPatientById(id: string) {
-    const urlApi = 'http://192.168.250.4:3000/patient/$id' ;
+    const urlApi = 'http://localhost:3000/patient/$id' ;
     return (this.patient = this.http.get(urlApi));
    }
    getPatientByDni(dni: string) {
-    const urlApi = `http://192.168.250.4:3000/patient/${dni}` ;
+    const urlApi = `http://localhost:3000/patient/${dni}` ;
     return (this.patient = this.http.get(urlApi));
    }
    getPiezaById(id: string) {
-    const urlApi = `http://192.168.250.4:3000/piezadental/pieza${id}` ;
+    const urlApi = `http://localhost:3000/piezadental/pieza${id}` ;
     return (this.patient = this.http.get(urlApi));
    }
    getUserByDni(dni: string) {
-    const urlApi = `http://192.168.250.4:3000/users/dni/${dni}` ;
+    const urlApi = `http://localhost:3000/users/dni/${dni}` ;
     return (this.patient = this.http.get(urlApi));
    }
    getAllMAterial() {
-    const urlApi = 'http://192.168.250.4:3000/material' ;
+    const urlApi = 'http://localhost:3000/material' ;
     return this.http.get(urlApi);
   }
   getMAterialByName(nombre: string) {
-    const urlApi = `http://192.168.250.4:3000/material/${nombre}` ;
+    const urlApi = `http://localhost:3000/material/${nombre}` ;
     return this.http.get(urlApi);
   }
   getInstrumentosByName(nombre: string) {
-    const urlApi = `http://192.168.250.4:3000/instrumento/${nombre}` ;
+    const urlApi = `http://localhost:3000/instrumento/${nombre}` ;
     return this.http.get(urlApi);
   }
   getAllPadecimientoPorPieza() {
-    const urlApi = 'http://192.168.250.4:3000/padecimientoporPieza' ;
+    const urlApi = 'http://localhost:3000/padecimientoporPieza' ;
     return this.http.get(urlApi);
   }
   getAllPiezasDentales() {
-    const urlApi = 'http://192.168.250.4:3000/piezadental' ;
+    const urlApi = 'http://localhost:3000/piezadental' ;
     return this.http.get(urlApi);
   }
   getAllProcedimientosTodoslosdientes() {
-    const urlApi = 'http://192.168.250.4:3000/procedimientoapadecimiento/todoslosdientes' ;
+    const urlApi = 'http://localhost:3000/procedimientoapadecimiento/todoslosdientes' ;
     return this.http.get(urlApi);
   }
   getAllodontogramasbypatient(id) {
-    const urlApi = `http://192.168.250.4:3000/odontograma/patient${id}` ;
+    const urlApi = `http://localhost:3000/odontograma/patient${id}` ;
     return this.http.get(urlApi);
   }
   getAllodontogramastodoslosdientesbypatient(id) {
-    const urlApi = `http://192.168.250.4:3000/seguimientotodosdientes/patientseguimiento${id}` ;
+    const urlApi = `http://localhost:3000/seguimientotodosdientes/patientseguimiento${id}` ;
     return this.http.get(urlApi);
   }
   getAllortodonciasbypatient(id) {
-    const urlApi = `http://192.168.250.4:3000/seguimientoortodoncia/odontograma/${id}` ;
+    const urlApi = `http://localhost:3000/seguimientoortodoncia/odontograma/${id}` ;
     return this.http.get(urlApi);
   }
   getAllodontogramasdeOrtodoncia(id) {
-    const urlApi = `http://192.168.250.4:3000/ortodoncia/patient/ortodoncia/${id}` ;
+    const urlApi = `http://localhost:3000/ortodoncia/patient/ortodoncia/${id}` ;
     return this.http.get(urlApi);
   }
   getAllprocedimientopornombre(nombre) {
-    const urlApi = `http://192.168.250.4:3000/procedimientoapadecimiento/nombreprocedimiento/${nombre}` ;
+    const urlApi = `http://localhost:3000/procedimientoapadecimiento/nombreprocedimiento/${nombre}` ;
     return this.http.get(urlApi);
   }
   getAllPadecimientosxpiezaporNomeclatura(nom: string) {
-    const urlApi = `http://192.168.250.4:3000/padecimientoporPieza/nomeclatura/${nom}` ;
+    const urlApi = `http://localhost:3000/padecimientoporPieza/nomeclatura/${nom}` ;
     return this.http.get(urlApi);
   }
   getAllServicios() {
-    const urlApi = 'http://192.168.250.4:3000/servicios' ;
+    const urlApi = 'http://localhost:3000/servicios' ;
     return this.http.get(urlApi);
   }
   getAllProcedimientos() {
-    const urlApi = 'http://192.168.250.4:3000/procedimiento' ;
+    const urlApi = 'http://localhost:3000/procedimiento' ;
     return this.http.get(urlApi);
   }
   getAllProcedimientosaPadecimientos() {
-    const urlApi = 'http://192.168.250.4:3000/procedimientoapadecimiento' ;
+    const urlApi = 'http://localhost:3000/procedimientoapadecimiento' ;
     return this.http.get(urlApi);
   }
   getAllProcedimientosaUNpadecimiento(padecimiento) {
-    const urlApi = `http://192.168.250.4:3000/procedimientoapadecimiento/procedimiento/${padecimiento}` ;
+    const urlApi = `http://localhost:3000/procedimientoapadecimiento/procedimiento/${padecimiento}` ;
     return this.http.get(urlApi);
   }
   getAllPadecimientos() {
-    const urlApi = 'http://192.168.250.4:3000/padecimiento' ;
+    const urlApi = 'http://localhost:3000/padecimiento' ;
     return this.http.get(urlApi);
   }
   getAllMAterialOrtodoncia() {
-    const urlApi = 'http://192.168.250.4:3000/material/ortodoncia' ;
+    const urlApi = 'http://localhost:3000/material/ortodoncia' ;
     return this.http.get(urlApi);
   }
   getAllMAterialOdontologia() {
-    const urlApi = 'http://192.168.250.4:3000/material/odontologia' ;
+    const urlApi = 'http://localhost:3000/material/odontologia' ;
     return this.http.get(urlApi);
   }
 
    getAllIntrumentos() {
-    const urlApi = 'http://192.168.250.4:3000/instrumento' ;
+    const urlApi = 'http://localhost:3000/instrumento' ;
     return this.http.get(urlApi);
   }
   getAllIntrumentosOrtodoncia() {
-    const urlApi = 'http://192.168.250.4:3000/instrumento/ortodoncia' ;
+    const urlApi = 'http://localhost:3000/instrumento/ortodoncia' ;
     return this.http.get(urlApi);
   }
   getAllIntrumentosOdontologia() {
-    const urlApi = 'http://192.168.250.4:3000/instrumento/odontologia' ;
+    const urlApi = 'http://localhost:3000/instrumento/odontologia' ;
     return this.http.get(urlApi);
   }
 }
